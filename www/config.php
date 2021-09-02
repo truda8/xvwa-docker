72a2cb37539df6970b3fcb9b3b443c9921a5fc33
truda8 <chenhaikun8@gmail.com>
2021-09-02 14:14:12 +0800

    first commit

---
+++ www/config.php
@@ -0,0 +1,10 @@
+<?php
+$XVWA_WEBROOT = "/www/wwwroot/xvwa/";
+$host = "xvwadb";
+$dbname = "xvwa";
+$user = "xvwa";
+$pass = "FSMuSI6yUV9yfi2aMZ31";
+$conn = new mysqli($host,$user,$pass,$dbname);
+$conn1 = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
